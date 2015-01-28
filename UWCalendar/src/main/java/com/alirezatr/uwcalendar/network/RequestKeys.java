00ea07a4d517ffaacc726decb29b0a9c3307924b
Ali Reza <alirezatr@hotmail.com>
2015-01-27 20:03:09 -0500

    Updated term number

--- UWCalendar/src/main/java/com/alirezatr/uwcalendar/network/RequestKeys.java
+++ UWCalendar/src/main/java/com/alirezatr/uwcalendar/network/RequestKeys.java
@@ -7,7 +7,7 @@ public class RequestKeys {
 
     public static final String API_KEY = "624b3a3eab10f59832f55b39f6900947";
     public static final String REQUEST_FORMAT = "json";
-    public static final String REQUEST_TERM = "1149";
+    public static final String REQUEST_TERM = "1151";
 
     public static final String COURSES_REQUEST_URL = "https://api.uwaterloo.ca/v2/courses/";
     public static final String CLASS_REQUEST_URL = "https://api.uwaterloo.ca/v2/terms/";
